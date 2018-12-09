f136f87c7d9b297006d23e02a0e48655d583c069
oystein barstad <oebars@gmail.com>
2018-12-09 21:36:21 +0100

    initial commit

---
+++ src/main/com/madsensoftworks/app/ws/ui/entrypoints/UsersEntryPoint.java
@@ -0,0 +1,12 @@
+package main.com.madsensoftworks.app.ws.ui.entrypoints;
+
+import javax.ws.rs.Path;
+
+@Path("/users")
+public class UsersEntryPoint {
+
+    public void createUser() {
+
+    }
+
+}
