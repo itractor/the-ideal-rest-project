dc0627fd14cbd8c18f62e9da6432ed147deb1dd7
oystein barstad <oebars@gmail.com>
2018-12-10 18:57:46 +0100

    Added request and response for userprofile

--- src/main/com/madsensoftworks/app/ws/ui/entrypoints/UsersEntryPoint.java
+++ src/main/com/madsensoftworks/app/ws/ui/entrypoints/UsersEntryPoint.java
@@ -1,11 +1,24 @@
 package main.com.madsensoftworks.app.ws.ui.entrypoints;
 
+import main.com.madsensoftworks.app.ws.ui.model.request.CreateUserRequestModel;
+import main.com.madsensoftworks.app.ws.ui.model.response.UserProfileRest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/users")
 public class UsersEntryPoint {
 
-    public void createUser() {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public UserProfileRest createUser(CreateUserRequestModel requestObject) {
+        UserProfileRest returnValue = new UserProfileRest();
+
+        return returnValue;
 
     }
 
